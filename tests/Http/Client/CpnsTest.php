0708ec0970cdb7c6f63418b9ace945dca4c1a3ad
Achmad Hadi Kurnia <imachmadhadikurnia@gmail.com>
2024-03-29 16:18:56 +0700

    Remove unused line

--- tests/Http/Client/CpnsTest.php
+++ tests/Http/Client/CpnsTest.php
@@ -1,17 +1,1 @@
 <?php
-
-use Kanekescom\Siasn\Simpeg\Api\Facades\Simpeg;
-
-// it('can post cpns save', function () {
-//     $query = json_decode(config('siasn-simpeg-api.params_test.post_cpns_save_query'), true);
-
-//     expect($query)->toBeArray();
-
-//     $response = Simpeg::postCpnsSave([], $query);
-//     $result = $response->collect()->toArray();
-
-//     expect($response->successful())->toBeTrue();
-//     expect($result)->toMatchArray([
-//         'success' => true,
-//     ]);
-// });
